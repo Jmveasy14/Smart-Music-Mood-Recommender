4a37da95dd1d378ece7279d73736a4f301bd889e
Joshua Veasy <Jmveasy14@gmail.com>
2025-05-21 16:36:24 -0400

    modified spotify routes and  homescreen

---
+++ server/routes/spotifyRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { getPlaylistData } = require('.../services/spotify');
+
+router.post('/playlist', async (req, res) => {
+    try {
+      const { playlistId, accessToken } = req.body;
+      const data = await getPlaylistData(playlistId, accessToken);
+      res.json(data);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+  
+  module.exports = router;
+  
